c4fc6a5af52b7f82961f6179826d2c6eefd92259
ymunt <yana.muntianu@gmail.com>
2020-02-03 20:25:23 +0200

    Task: Create a class Vector3D Properties: X, Y, Z and Length as double and public scope and accessors X, Y and Z will be set by test methods Length should only have an get-accessor which calculates the vector's length
    Link to Kata https://www.codewars.com/kata/582b079bbbbc74bed7000095/train/csharp

---
+++ src/main/java/codewars/tasks/java/Main.java
@@ -0,0 +1,11 @@
+package codewars.tasks.java;
+
+
+public class Main {
+
+    public static void main (String [] args){
+        Vector3D vector = new Vector3D();
+        vector.getLength(0,2,0);
+
+    }
+}
---
+++ src/test/java/codewars/tasks/java/Vector3DTest.java
@@ -0,0 +1,29 @@
+package codewars.tasks.java;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class Vector3DTest {
+    @BeforeClass
+    public void testSetUp(){
+        System.out.println("Running TestNG");
+    }
+    @AfterClass
+    public void testTearDown(){
+        System.out.println("Stopping TestNG");
+    }
+
+    @Test
+    public void countVectorLength(){
+        System.out.println("@Test - countVectorLength");
+        Vector3D vector3D = new Vector3D();
+
+        vector3D.getLength(0,2,0);
+        assertEquals(2.0, 7.0);
+
+
+    }
+
+}
