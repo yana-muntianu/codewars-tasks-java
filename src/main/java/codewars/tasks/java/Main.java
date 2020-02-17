87dd1998975e56efb3de72a24e2b5b8fa1278bbb
ymunt <yana.muntianu@gmail.com>
2020-02-17 16:22:24 +0200

    uncomment in Main class

--- src/main/java/codewars/tasks/java/Main.java
+++ src/main/java/codewars/tasks/java/Main.java
@@ -1,24 +1,29 @@
 package codewars.tasks.java;
 
 
+import codewars.tasks.java.battle.Battle;
+import codewars.tasks.java.battle.Fighter;
+import codewars.tasks.java.block.Block;
 import codewars.tasks.java.dictionary.Dictionary;
+import codewars.tasks.java.leapYears.LeapYears;
+import codewars.tasks.java.vector.Vector3D;
 
 public class Main {
 
     public static void main (String [] args){
 
-//        Vector3D vector = new Vector3D();
-//        vector.getLength(0,2,0);
-//
-//        Block block = new Block(new int[]{1,2,4});
-//        int volume = block.getVolume();
-//        int surfaceArea = block.getSurfaceArea();
-//
-//        LeapYears isYearLeap = new LeapYears();
-//        isYearLeap.isYearLeap(2004);
-//
-//        Battle.declareWinner(new Fighter("Lew", 10, 2),
-//                new Fighter("Harry", 5, 4), "Lew");
+        Vector3D vector = new Vector3D();
+        vector.getLength(0,2,0);
+
+        Block block = new Block(new int[]{1,2,4});
+        int volume = block.getVolume();
+        int surfaceArea = block.getSurfaceArea();
+
+        LeapYears isYearLeap = new LeapYears();
+        isYearLeap.isYearLeap(2004);
+
+        Battle.declareWinner(new Fighter("Lew", 10, 2),
+                new Fighter("Harry", 5, 4), "Lew");
 
         Dictionary d = new Dictionary();
 //        d.newEntry("Apple", "A fruit that grows on trees");
