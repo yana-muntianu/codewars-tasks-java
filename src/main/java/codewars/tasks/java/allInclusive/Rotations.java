5a59ee1ff6388a878a8ad182d439bcd3cf46a64b
ymunt <yana.muntianu@gmail.com>
2020-02-24 19:17:09 +0200

    partially working solution

--- src/main/java/codewars/tasks/java/allInclusive/Rotations.java
+++ src/main/java/codewars/tasks/java/allInclusive/Rotations.java
@@ -1,35 +1,42 @@
 package codewars.tasks.java.allInclusive;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Rotations {
 
     public static boolean containAllRots(String string, List<String> arr) {
 
-        int len = string.length();
-//        String [] array = new String[]{};
-
         /*
         copy the second part of the string in the temporary string and then
         copy the first part of the original string to the temporary string.
          */
+        boolean isAllRots = false;
+        int n = string.length();
+        List<String> retval = new ArrayList<>();
 
-        for(int i=0; i<len; i++) {
-            for (int j = i + 1; j < len; j++)
-                System.out.println(string.charAt(j));
-//                while (i <= len){
-//                    int [] array = new int[len];
-//                    array[0] = string.charAt(j);
-//
-//                }
+        if (!string.isEmpty()){
+            for(int i=0; i<n; i++){
+                StringBuilder sb = new StringBuilder();
+                for(int j=i+1; j<n; j++)
+                    sb.append(string.charAt(j));
+                for(int k=0; k<=i; k++)
+                    sb.append(string.charAt(k));
+                retval.add(sb.toString());
+            }
+        }else if (!arr.isEmpty()){
+            for (String element:retval
+                 ) {
+                if (!arr.contains(element)){
+                    isAllRots = false;
+                    break;
+                }else isAllRots = true;
+            }
 
-            for (int k = 0; k <= i; k++)
-                System.out.println(string.charAt(k));
+        }else
+             isAllRots = true;
+        System.out.println(isAllRots);
 
-        }
-
-
-
-        return true;
+        return isAllRots;
     }
 }
