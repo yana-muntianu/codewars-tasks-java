1323d08f5b5aeefc189ddb1c05ccc9effd91baba
Yana Muntianu <42188095+yana-muntianu@users.noreply.github.com>
2020-02-27 19:35:25 +0200

    Link to the Kata: https://www.codewars.com/kata/57ed4cef7b45ef8774000014/solutions/java (#8)
    Every now and then people in the office moves teams or departments. Depending what people are doing with their time they can become more or less boring. Time to assess the current team.
    
    You will be provided with an array of Person objects with each instance containing the name and department for a staff member.
    
    Depending on the cumulative score of the team, return the appropriate sentiment:
    
    <=80: 'kill me now'
    < 100 & > 80: 'i can handle this'
    100 or over: 'party time!!'

--- src/main/java/codewars/tasks/java/Main.java
+++ src/main/java/codewars/tasks/java/Main.java
@@ -1,10 +1,9 @@
 package codewars.tasks.java;
 
 
-import codewars.tasks.java.arithmeticFunction.ArithmeticFunction;
 import codewars.tasks.java.battle.Battle;
-import codewars.tasks.java.battle.Fighter;
 import codewars.tasks.java.block.Block;
+import codewars.tasks.java.boredomScore.TheOffice;
 import codewars.tasks.java.leapYears.LeapYears;
 import codewars.tasks.java.vector.Vector3D;
 
@@ -13,19 +12,16 @@ public class Main {
     public static void main (String [] args){
 
         Vector3D vector = new Vector3D();
-        vector.getLength(0,2,0);
 
         Block block = new Block(new int[]{1,2,4});
-        int volume = block.getVolume();
-        int surfaceArea = block.getSurfaceArea();
 
         LeapYears isYearLeap = new LeapYears();
-        isYearLeap.isYearLeap(2004);
 
-        Battle.declareWinner(new Fighter("Lew", 10, 2),
-                new Fighter("Harry", 5, 4), "Lew");
+        Battle battle = new Battle();
 
-        ArithmeticFunction function = new ArithmeticFunction();
+        TheOffice office = new TheOffice();
+
+
 
     }
 }
---
+++ src/main/java/codewars/tasks/java/boredomScore/Person.java
@@ -0,0 +1,15 @@
+package codewars.tasks.java.boredomScore;
+
+/*
+ ** an array of Person objects with each instance containing the name and department for a staff member.
+ */
+public class Person {
+    public final String name;        // name of the staff member
+    public final String department;  // department they work in
+
+    public Person(String name, String department){
+        this.name = name;
+        this.department = department;
+
+    }
+}
