abbc093b741a2bba4a25b0ac1361a9f25ea45c60
Yana Muntianu <42188095+yana-muntianu@users.noreply.github.com>
2020-02-27 18:53:50 +0200

    Temperature analysis I (#12)
    * Temperature analysis I
    
    Link to Kata: https://www.codewars.com/kata/588e0f11b7b4a5b373000041/train/csharp
    
    You were given a string of integer temperature values. Create a function lowest_temp(t) and return the lowest value or None/null/Nothing if the string is empty.
    
    - added Action package
    - comments resolved

---
+++ src/main/java/codewars/tasks/java/action/TemperatureAnalysisExecution.java
@@ -0,0 +1,11 @@
+package codewars.tasks.java.action;
+
+import codewars.tasks.java.temperatureAnalysis.TemperatureAnalysis;
+
+public class TemperatureAnalysisExecution {
+
+    public static void main (String [] args){
+
+        TemperatureAnalysis analysis = new TemperatureAnalysis();
+    }
+}
