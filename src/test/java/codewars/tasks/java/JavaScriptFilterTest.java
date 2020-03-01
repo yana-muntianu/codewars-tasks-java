df50aa2418fc8bb95390a1923f3cc1cddcfd7ac1
ymunt <yana.muntianu@gmail.com>
2020-03-01 17:31:59 +0200

    Javascript filter - 1 Link to kata: https://www.codewars.com/kata/525d9b1a037b7a9da7000905/train/csharp
    While developing a website, you detect that some of the members have troubles logging in. Searching through the code you find that all logins ending with a "_" make problems. So you want to write a function that takes an array of pairs of login-names and e-mails, and outputs an array of all login-name, e-mails-pairs from the login-names that end with "_".

---
+++ src/test/java/codewars/tasks/java/JavaScriptFilterTest.java
@@ -0,0 +1,36 @@
+package codewars.tasks.java;
+
+import codewars.tasks.java.javascriptFilter.JavaScriptFilter;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+
+public class JavaScriptFilterTest {
+
+    @Test(testName = "12", description = "Check emails with '_'")
+    public void testVerifyEmails(){
+
+        JavaScriptFilter filter = new JavaScriptFilter();
+
+        //1 email found
+        String[][] input = { new String[]{ "foo", "foo@foo.com" }, new String[]{ "bar_", "bar@bar.com" } };
+        String[][] act = {new String[] { "bar_", "bar@bar.com" }};
+        assertEquals(act, filter.verifyEmails(input));
+
+        //2 emails found
+        String[][] input1 = { new String[]{  "foo_", "foo@foo.com" }, new String[]{ "bar_", "bar@bar.com" } };
+        String[][] exp1 = {new String[]{ "foo_", "foo@foo.com"}, new String[] { "bar_", "bar@bar.com" }};
+        assertEquals(exp1, filter.verifyEmails(input1));
+
+        //no matching
+        String[][] input2 = { new String[]{  "foo", "foo@foo.com" }, new String[]{ "bar", "bar@bar.com" } };
+        String[][] exp2 = new String[0][];
+        assertEquals(exp2, filter.verifyEmails(input2));
+
+        //2 emails from 4 matched
+        String[][] input3 = { new String[]{  "foo_", "foo@foo.com" }, new String[]{ "bar_", "bar@bar.com" },
+            new String[]{ "may", "may@may.com" }, new String[]{ "bee", "bee@bee.com" }};
+        String[][] exp3 = {new String[]{ "foo_", "foo@foo.com"}, new String[] { "bar_", "bar@bar.com" }};
+        assertEquals(exp3, filter.verifyEmails(input3));
+
+    }
+}
