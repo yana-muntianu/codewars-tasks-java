2ff4e1851a628868ae6c38e6668a58bb0c2f55c3
ymunt <yana.muntianu@gmail.com>
2020-04-05 21:01:53 +0300

    Playing with Cubes III
    Link to Kata: https://www.codewars.com/kata/55c1d76ccc8f89fb2900009b/solutions/csharp
    
    Hey Cube-Lovers!
    
    Now we want to provide some more information about cubes. First, we implement two new properties: Volume and Surface.
    
    Additional to this task we are going to refactor our code, so it will be bit more like professional C# code! You change the variable Side to a public property and remove the 'Getter' and 'Setter'.
    
    The properties Volume and Surface should each have a public get-accessor and private set-accessor. Their correct mathematical values should be set in the cubes constructor and if Side's values is changed.
    
    This MSDN site should provide some help:
    https://msdn.microsoft.com/en-us/library/w86s7x04(v=VS.120).aspx

--- src/main/java/codewars/tasks/java/playingwithcubes/Cube.java
+++ src/main/java/codewars/tasks/java/playingwithcubes/Cube.java
@@ -1,35 +1,43 @@
 package codewars.tasks.java.playingwithcubes;
 
-public class Cube {
+public class Cube {}
 
-    public int side;
-    private int volume;
-    private int surface;
-
-    public Cube(){
-
-    }
-
-    public Cube(int side)
-    {
-        this.side = side;
-        this.volume = (side*side*side);
-        this.surface = 6*(side*side);
-    }
-
-    public int getVolume() {
-        return this.volume;
-    }
-
-    private void setVolume(int volume) {
-        this.volume = Math.abs(volume);
-    }
-
-    public int getSurface() {
-        return this.surface;
-    }
-
-    private void setSurface(int surface) {
-        this.surface = Math.abs(surface);
-    }
-}
+//using System;
+//
+//public class Cube
+//{
+//    private int side;
+//
+//    public int Side
+//    {
+//        get{return Math.Abs(side);}
+//        set{side = Math.Abs(value);}
+//    }
+//
+//    private int volume;
+//
+//    public int Volume
+//    {
+//        get {return Math.Abs(side*side*side);}
+//        private set {volume = Math.Abs(value);}
+//    }
+//
+//    private int surface;
+//
+//    public int Surface
+//    {
+//        get {return Math.Abs(6*(side*side));}
+//        private set {surface = Math.Abs(value);}
+//
+//    }
+//
+//    public Cube(){ }
+//
+//    public Cube(int cubeSide){
+//
+//        this.side = cubeSide;
+//        this.volume = (this.side*this.side*this.side);
+//        this.surface = 6*(this.side*this.side);
+//
+//
+//    }
