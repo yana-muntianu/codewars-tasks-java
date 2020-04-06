6ad6cfde106e5c16cf2d2433783ebcb0ae613300
Yana Muntianu <42188095+yana-muntianu@users.noreply.github.com>
2020-04-06 22:15:37 +0300

    Persistent Bugger (#20)
    Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence,
     which is the number of times you must multiply the digits in num until you reach a single digit.
    
     For example:
    persistence(39) == 3  // because 3*9 = 27, 2*7 = 14, 1*4=4 and 4 has only one digit
    persistence(999) == 4 // because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2
    persistence(4) == 0   // because 4 is already a one-digit number

---
+++ src/main/java/codewars/tasks/java/persictentbugger/Persist.java
@@ -0,0 +1,28 @@
+package codewars.tasks.java.persictentbugger;
+
+public class Persist {
+   /* Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence,
+        * which is the number of times you must multiply the digits in num until you reach a single digit.
+        *
+        * For example:
+            *
+            * # persistence(39) == 3  // because 3*9 = 27, 2*7 = 14, 1*4=4 and 4 has only one digit
+            * # persistence(999) == 4 // because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2
+            * # persistence(4) == 0   // because 4 is already a one-digit number
+            */
+
+    public static int persistence(long num) {
+        int result = 0;
+
+        while (num > 9) {
+            long multiply = 1;
+            for (char digit : String.valueOf(num).toCharArray()) {
+                multiply *= Long.parseLong(String.valueOf(digit));
+            }
+            num = multiply;
+            result++;
+        }
+
+        return result;
+    }
+}
