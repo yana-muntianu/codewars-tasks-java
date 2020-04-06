4449d90501c885707935a77d34bd83691a9d0aa8
Yana Muntianu <42188095+yana-muntianu@users.noreply.github.com>
2020-04-06 22:13:49 +0300

    L3375p34k (#16)
    The objective of this Kata is to provide some way to encode a string into its leetspeak version.
    Link to Kata: https://www.codewars.com/kata/543bfbecdef6345f52000e4d/java

---
+++ src/main/java/codewars/tasks/java/action/LeetSpeakExecution.java
@@ -0,0 +1,12 @@
+package codewars.tasks.java.action;
+
+import codewars.tasks.java.leetspeak.Leetspeak;
+
+public class LeetSpeakExecution {
+    public static void main(String[] args) {
+
+        Leetspeak myEncoder = new Leetspeak();
+        myEncoder.encode("lemon");
+
+    }
+}
