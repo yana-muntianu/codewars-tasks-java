c09d98c77a0ffd9399a08e16589da0079b002753
Yana Muntianu <42188095+yana-muntianu@users.noreply.github.com>
2020-04-06 22:15:02 +0300

    Sortable Shapes (#19)
    * Sortable Shapes
    
    Link to Kata: https://www.codewars.com/kata/586669a8442e3fc307000048/train/java
    
    * Sortable Shapes
    
    Create different shapes that can be part of a sortable list. The sort order is based on the size of their respective areas:
    The area of a Square is the square of its side
    The area of a Rectangle is width multiplied by height
    The area of a Triangle is base multiplied by height divided by 2
    The area of a Circle is the square of its radius multiplied by π
    The area of a CustomShape is given
    
    Link to Kata: https://www.codewars.com/kata/586669a8442e3fc307000048/train/java

---
+++ src/main/java/codewars/tasks/java/sortableShapes/Rectangle.java
@@ -0,0 +1,9 @@
+package codewars.tasks.java.sortableShapes;
+
+public class Rectangle extends Shape {
+
+    public Rectangle(double width, double height) {
+        super(width * height);
+    }
+
+}
